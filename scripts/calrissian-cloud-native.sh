97d5692f809b11a722dacf027be508af91126a88
Fabrice Brito <fabrice.brito@terradue.com>
2024-10-21 16:12:41 +0200

    add workflow id to CWL

--- scripts/calrissian-cloud-native.sh
+++ scripts/calrissian-cloud-native.sh
@@ -12,7 +12,7 @@ calrissian \
     --usage-report /calrissian/usage.json \
     --tool-logs-basepath /calrissian/logs \
     --pod-nodeselectors /etc/calrissian/pod-node-selector.yaml \
-    /workspace/mastering-app-package/runs/app-water-bodies-cloud-native.${version}.cwl \
+    /workspace/mastering-app-package/runs/app-water-bodies-cloud-native.${version}.cwl#water-bodies \
     --stac_items "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20210708_0_L2A" \
     --stac_items "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2B_10TFK_20210713_0_L2A" \
     --stac_items "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20210718_0_L2A" \
